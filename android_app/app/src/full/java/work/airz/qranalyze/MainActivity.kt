79b2e9215b432165c4f9197ad342295bcd1232f0
khrom <devs@outlook.jp>
2019-04-07 19:43:50 +0900

    [add] bitmap 取得部分を追加

--- android_app/app/src/full/java/work/airz/qranalyze/MainActivity.kt
+++ android_app/app/src/full/java/work/airz/qranalyze/MainActivity.kt
@@ -4,7 +4,9 @@ import android.Manifest
 import android.content.*
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Environment
 import android.support.v4.app.ActivityCompat
@@ -15,12 +17,17 @@ import android.view.LayoutInflater
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.google.zxing.*
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 import com.journeyapps.barcodescanner.BarcodeCallback
 import com.journeyapps.barcodescanner.BarcodeResult
 import com.journeyapps.barcodescanner.DecoratedBarcodeView
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 import java.nio.charset.Charset
 import java.util.*
 
@@ -28,7 +35,8 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private val REQUEST_PERMISSION = 1000
     private lateinit var qrReaderView: DecoratedBarcodeView
-
+    private var lookupMap = hashMapOf<String, String>()
+    private var urlMap = hashMapOf<String, String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +46,7 @@ class MainActivity : AppCompatActivity() {
             Thread.sleep(1000)
         }
         Log.d("permission", "granted")
+        loadLookup()
         readQR()
     }
 
@@ -65,6 +74,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
         qrReaderView.resume()
+
     }
 
 
@@ -117,6 +127,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun loadLookup() {
+        BufferedReader(InputStreamReader(resources.openRawResource(R.raw.lookup_table))).use {
+            var line = it.readLines()
+            line.forEach { line ->
+                lookupMap[line.split(",")[0]] = line.split(",")[1]
+            }
+        }
+        BufferedReader(InputStreamReader(resources.openRawResource(R.raw.urls))).use {
+            var line = it.readLines()
+            line.forEach { line ->
+                urlMap[line.split(",")[0]] = line.split(",")[1]
+            }
+        }
+        Toast.makeText(applicationContext,"データを読み込みました。",Toast.LENGTH_LONG)
+        Log.d("aaaa","データを読み込みました")
+    }
+
 
     fun saveAlert(qrImage: Bitmap, readerView: DecoratedBarcodeView, qrstring: String) {
 
@@ -130,9 +157,7 @@ class MainActivity : AppCompatActivity() {
         var editText = dialogRoot.findViewById<EditText>(R.id.filename)
         dialogRoot.findViewById<TextView>(R.id.hexall).text = qrstring
         editText.setText(qrstring.replace("\\s".toRegex(), "").substring(6, 18))
-
         var idText = dialogRoot.findViewById<TextView>(R.id.id)
-
 
         var builder = AlertDialog.Builder(this)
         builder.setView(dialogRoot)
@@ -157,6 +182,17 @@ class MainActivity : AppCompatActivity() {
             readerView.resume()
         })
         builder.show()
+
+        val hexStr = editText.text.toString().toLowerCase()
+        if (lookupMap.containsKey(hexStr)) {
+            Toast.makeText(applicationContext,"すでにあるっぽい",Toast.LENGTH_SHORT)
+            Log.d("aaa","すでにあるっぽい")
+            ImageAsyncTask().execute(urlMap[lookupMap[hexStr]], imageView)
+            idText.text = lookupMap[hexStr]
+        }else{
+            Toast.makeText(applicationContext,"あたらしいやつ",Toast.LENGTH_SHORT)
+            Log.d("aaa","あたらしいやつ")
+        }
     }
 
     private fun checkPermission(): Boolean {
@@ -193,6 +229,39 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         qrReaderView.pause()
+    }
+
+    private fun setImage(url: String, thumbnail: ImageView) {
+        ImageAsyncTask().execute(url, thumbnail)
+    }
+
+    companion object {
+        class ImageAsyncTask : AsyncTask<Any, Void, Bitmap>() {
+            var imageView: ImageView? = null
+
+            override fun doInBackground(vararg params: Any): Bitmap? {
+                val url = URL(params[0] as? String ?: return null)
+                imageView = params[1] as? ImageView ?: return null
+                try {
+                    val urlConnection = url.openConnection()  as? HttpURLConnection ?: return null
+                    urlConnection.readTimeout = 5000
+                    urlConnection.connectTimeout = 7000
+                    urlConnection.requestMethod = "GET"
+                    urlConnection.doInput = true
+                    urlConnection.connect()
+                    return BitmapFactory.decodeStream(urlConnection.inputStream)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+                return null
+            }
+
+            override fun onPostExecute(result: Bitmap?) {
+                super.onPostExecute(result)
+                result ?: return
+                imageView!!.setImageBitmap(result)
+            }
+        }
     }
 
 
